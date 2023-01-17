b39042d47d6703df911350051d2c46175bc3df23
akshaya002UWL <104349647+akshaya002UWL@users.noreply.github.com>
2023-01-17 12:20:41 +0530

    removing %20

--- app.py
+++ app.py
@@ -221,8 +221,8 @@ def update_JDAndComp():
         key = next(iter(input_data))
         input_josn = input_data[key]
         print(input_josn)
-        input_josn['hiringManager'] = request.args.get("HiringManager")
-        input_josn['recruiter'] = request.args.get("Recruiter")
+        input_josn['hiringManager'] = request.args.get("HiringManager").replace('%20+', '')
+        input_josn['recruiter'] = request.args.get("Recruiter").replace('%20+', '')
         print("request.args.get(HiringManager) = " + request.args.get("HiringManager"))
         print("request.args.get(Recruiter) = " + request.args.get("Recruiter"))
 
